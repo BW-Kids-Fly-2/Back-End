b36b9669c7d01b33090b393a367ec95fa3b1660f
christina kaliora <develop@pirron.one>
2020-03-04 21:10:32 +0100

    changed db-config

--- database/db-config.js
+++ database/db-config.js
@@ -1,6 +1,7 @@
 const knex = require("knex");
 const config = require("../knexfile.js");
 
-const environment = process.env.DB_ENV || "development";
+//const environment = process.env.DB_ENV || "development";
+const environment = "production";
 
 module.exports = knex(config[environment]);
