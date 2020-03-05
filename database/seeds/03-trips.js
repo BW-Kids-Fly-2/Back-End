7c72fefe3f0885fa81950e752c2c5c33e4341c7e
christina kaliora <develop@pirron.one>
2020-03-05 15:51:49 +0100

    changes on seeds

--- database/seeds/03-trips.js
+++ database/seeds/03-trips.js
@@ -10,50 +10,25 @@ exports.seed = function(knex) {
             id: 1,
             trips_parent_id: 1,
             trips_assistant_id: 1,
-            trip_name: "Trip to the edge of the galaxy.",
+            trip_name: "Mars",
             kids_traveling: 2,
             checked_bags: 3,
             carryon_bags: 3,
             carseats: 1,
             strollers: 1,
-            notes: "They've gone plaid!"
+            notes: "Test notes!!!"
           },
           {
             id: 2,
             trips_parent_id: 2,
             trips_assistant_id: 2,
-            trip_name: "Trip to Spaceball One",
+            trip_name: "Venus",
             kids_traveling: 3,
             checked_bags: 5,
             carryon_bags: 5,
             carseats: 2,
             strollers: 2,
-            notes:
-              "There's only one person I know that would give me the raspberry!"
-          },
-          {
-            id: 3,
-            trips_parent_id: 3,
-            trips_assistant_id: null,
-            trip_name: "Trip to Druidia",
-            kids_traveling: 2,
-            checked_bags: 3,
-            carryon_bags: 3,
-            carseats: 1,
-            strollers: 1,
-            notes: "Just what I needed... a Druish princess."
-          },
-          {
-            id: 4,
-            trips_parent_id: 1,
-            trips_assistant_id: null,
-            trip_name: "Going to see Yogurt",
-            kids_traveling: 1,
-            checked_bags: 2,
-            carryon_bags: 2,
-            carseats: 1,
-            strollers: 1,
-            notes: "Use the schwartz, Lonestar."
+            notes: "Test notes!!!"
           }
         ]);
       })
--- database/seeds/04-flights.js
+++ database/seeds/04-flights.js
@@ -67,68 +67,6 @@ exports.seed = function(knex) {
 
             en_route_dep_airport: true,
             en_route_arr_airport: false
-          },
-          {
-            id: 3,
-            flight_info_assistant_id_dep: null,
-            flight_info_assistant_id_arr: null,
-
-            flight_info_parent_id: 3,
-            flight_info_trips_id: 3,
-
-            dep_airport: "JFK",
-            arr_airport: "MIA",
-
-            airline: "Spaceball Air",
-
-            dep_flight_num: "1234",
-            arr_flight_num: "1212",
-
-            dep_time: "08:30",
-            arr_time: "11:54",
-
-            help_req_at_dep: true,
-            help_req_at_arr: false,
-
-            asst_sched_dep_airport: true,
-            asst_sched_arr_airport: false,
-
-            arrived_dep_airport: false,
-            arrived_arr_airport: false,
-
-            en_route_dep_airport: true,
-            en_route_arr_airport: false
-          },
-          {
-            id: 4,
-            flight_info_assistant_id_dep: null,
-            flight_info_assistant_id_arr: null,
-
-            flight_info_parent_id: 3,
-            flight_info_trips_id: 3,
-
-            dep_airport: "JFK",
-            arr_airport: "MIA",
-
-            airline: "Spaceball Air",
-
-            dep_flight_num: "1234",
-            arr_flight_num: "1212",
-
-            dep_time: "15:30",
-            arr_time: "18:21",
-
-            help_req_at_dep: true,
-            help_req_at_arr: false,
-
-            asst_sched_dep_airport: true,
-            asst_sched_arr_airport: false,
-
-            arrived_dep_airport: false,
-            arrived_arr_airport: false,
-
-            en_route_dep_airport: true,
-            en_route_arr_airport: false
           }
         ]);
       })
